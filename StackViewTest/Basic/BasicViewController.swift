9ed2d4ffec2faed5647ba90bbe77834798cb08b3
Yamada Shunya <s-yamada@finewerk.co.jp>
2019-05-10 10:53:31 +0900

    Update basic view

--- StackViewTest/Basic/BasicViewController.swift
+++ StackViewTest/Basic/BasicViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class BasicViewController: UIViewController {
+class BasicViewController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()

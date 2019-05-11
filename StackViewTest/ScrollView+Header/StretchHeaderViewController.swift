9155273991bea017a7fbe30b55a1c937ea2bd82b
hayabusabusa <31949692+hayabusabusa@users.noreply.github.com>
2019-05-11 20:51:09 +0900

    Add stretch header view

---
+++ StackViewTest/ScrollView+Header/StretchHeaderViewController.swift
@@ -0,0 +1,16 @@
+//
+//  StretchHeaderViewController.swift
+//  StackViewTest
+//
+//  Created by 山田隼也 on 2019/05/11.
+//  Copyright © 2019 Yamada Shunya. All rights reserved.
+//
+
+import UIKit
+
+class StretchHeaderViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
